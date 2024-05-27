c399479eb6e731ca2a1f51df3c0594c255a8298d
sarthakg11 <127665657+sarthakg11@users.noreply.github.com>
2024-05-27 20:47:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Key Pair/key-pair.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+public:
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    unordered_set<int> st;
+	    for(int i=0;i<n;i++){
+	        if(st.find(x-arr[i])!=st.end()) return true;
+	        else st.insert(arr[i]);
+	    }
+	    return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
