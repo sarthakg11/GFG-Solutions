2f6c0cca54b221e04382b50d8fa6cd10c2886670
sarthakg11 <127665657+sarthakg11@users.noreply.github.com>
2024-05-29 10:25:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        map<int,int> answer;
+        int pairs=0;
+        for(int i=0;i<n;i++){
+            if(answer.find(k-arr[i])!=answer.end()) pairs+=answer[(k-arr[i])];
+            answer[arr[i]]++;
+        }
+        return pairs;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
